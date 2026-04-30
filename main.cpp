0fc31ee93995bff38f122ae535a7f9946130f88e
dmalone25 <dmalone0055@gmail.com>
2026-04-30 10:32:37 -0700

    Run tests

--- main.cpp
+++ main.cpp
@@ -1,28 +1,10 @@
 #include <iostream>
+#include <vector>
 
 #include "sudoku.hpp"
+// #include "sudoku_tests.cpp"
 
 int main() {
-    Sudoku s;
-    s.set_space(0, 0, 1);
-    s.set_space(0, 1, 2);
-    s.set_space(0, 2, 3);
-    s.set_space(1, 0, 4);
-    s.set_space(1, 1, 5);
-    s.set_space(1, 2, 6);
-    s.set_space(2, 0, 7);
-    s.set_space(2, 1, 8);
-    s.set_space(2, 2, 9);
-    s.print();
-
-    // auto square = s.get_square(0);
-    // auto row = s.get_row(0);
-    // for (auto i : row) {
-    //     std::cout << i << "\n";
-    // }
-    // auto column = s.get_column(0);
-    // for (auto i : column) {
-    //     std::cout << i << "\n";
-    // }
+    // run_tests();
     return 0;
 }
